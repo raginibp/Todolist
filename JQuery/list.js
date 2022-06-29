1b7842ea39d53db7cfe09a75fc6dd3cd84d87c28
raginibp <raginipatel155@gmail.com>
2022-06-29 10:07:30 +0530

    All files added

---
+++ JQuery/list.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#Enter").click(function(){
+        var text1 = $("#text1").val();
+         let done=$(":button").val();
+        $('#displayArea').append("<tr><td>" + text1 + "</td><td>" +
+        '<button class="done">Done</button><button class="undone">Undone</button><button class="delete">Delete</button>' +
+         "</td><td class='result'></td</tr>")
+       // $("input[type=text], textarea").val("");
+    });
+    // $(document).on('click','.delete',function(){
+    //   $(this).parent().parent().remove();
+      
+    // });
+
+    $(document).on('click','.delete',function(){
+      //var i=1;
+       i = myTable.rows.length;
+    
+      if(i>2){
+          // var row = btn.parentNode.parentNode;
+          $(this).parent().parent().remove();
+      }
+    });
+  
+  
+    $(document).on('click','.done',function(){
+      $(this).parent().siblings('.result').html('&#9989;');
+      $(this).siblings('.delete').attr("disabled",true)
+    });
+    $(document).on('click','.undone',function(){
+      $(this).parent().siblings('.result').html('&#x274C;');
+      $(this).siblings('.delete').attr("disabled",false)
+    });
+  });
